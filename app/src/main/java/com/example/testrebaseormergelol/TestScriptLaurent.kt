741ae8b3d7d1a183d1bac276993971b3b54420d7
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2024-02-08 18:51:22 +0100

    test push burger

--- app/src/main/java/com/example/testrebaseormergelol/TestScriptLaurent.kt
+++ app/src/main/java/com/example/testrebaseormergelol/TestScriptLaurent.kt
@@ -4,7 +4,7 @@ package com.example.testrebaseormergelol
     private lateinit var nameOfFood:String
 
     fun main(){
-        nameOfFood="Pizza"
+        nameOfFood="Burger"
         for (i in 1..7){
             if(i <=4){
                 println("$nameOfFood - $i")
