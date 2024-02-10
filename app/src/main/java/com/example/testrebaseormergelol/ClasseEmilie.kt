81c9b82025f6cec12b6823af9995d556be448aa9
Émilie P <emiliep.dev@gmail.com>
2024-02-10 16:19:02 +0100

    Added sayHi() method to ClasseEmilie

--- app/src/main/java/com/example/testrebaseormergelol/ClasseEmilie.kt
+++ app/src/main/java/com/example/testrebaseormergelol/ClasseEmilie.kt
@@ -1,4 +1,12 @@
 package com.example.testrebaseormergelol
 
 class ClasseEmilie {
+
+    fun main() {
+        sayHi()
+    }
+
+    private fun sayHi() {
+        println("Kikou ! Premier commit sur la branche emilie_branch_1")
+    }
 }
