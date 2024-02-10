71401a12efe9f8a9c867b0d1f16421af88aed027
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2024-02-10 09:40:13 +0100

    deleted code in ClasseLoran

--- app/src/main/java/com/example/testrebaseormergelol/ClasseLoran.kt
+++ app/src/main/java/com/example/testrebaseormergelol/ClasseLoran.kt
@@ -1,17 +1,7 @@
 package com.example.testrebaseormergelol
 
 class ClasseLoran {
-    private lateinit var nameOfFood:String
 
-    fun tuMangesQuoi(){
-
-        nameOfFood="Ramen"
-        println(nameOfFood)
-    }
-fun main(){
-    val classTest=ClasseLoran()
-    classTest.tuMangesQuoi()
-}
 
 
 }
